52face37b099e22783606992ddfa938df5f3363d
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2025-02-08 20:58:12 +0500

    внес  MIME

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
+  server: {
+    mimeTypes: {
+      "text/css": ["css"],
+    },
+  },
   plugins: [
     viteStaticCopy({
       targets: [
-        { src: 'src/pages', dest: '' }, // Копируем все страницы в dist/
-        { src: 'src/components', dest: '' }, // Копируем компоненты в dist/
-        { src: 'src/js/include.js', dest: 'js' } // Копируем include.js в dist/js/
-      ]
-    })
-  ]
+        { src: "src/pages", dest: "" }, // Копируем все страницы в dist/
+        { src: "src/components", dest: "" }, // Копируем компоненты в dist/
+        { src: "src/js/include.js", dest: "js" }, // Копируем include.js в dist/js/
+      ],
+    }),
+  ],
 });
