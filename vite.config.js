4994c6c074c86ba237fac768184a2db7c7263f96
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2025-02-08 20:44:07 +0500

    меняем пути

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
     viteStaticCopy({
-      targets: [{ src: "src/js/include.js", dest: "js" }],
-    }),
-  ],
+      targets: [
+        { src: 'src/pages', dest: '' }, // Копируем все страницы в dist/
+        { src: 'src/components', dest: '' } // Копируем компоненты в dist/
+      ]
+    })
+  ]
 });
