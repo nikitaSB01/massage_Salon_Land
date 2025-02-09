063fb263f07a6acc188489bd027d8d10f742f815
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2025-02-09 16:46:57 +0500

    add in vite reviews

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
       targets: [
         { src: "src/components", dest: "" }, // Копируем компоненты
         { src: "src/js/include.js", dest: "js" }, // Копируем include.js
+        { src: "src/js/reviews.js", dest: "js" }, // Копируем include.js
         { src: "src/js/service-details.js", dest: "js" }, // Добавили service-details.js
       ],
     }),
