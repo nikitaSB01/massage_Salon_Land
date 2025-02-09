f42ada475deb4bfd4d5928203f92001444348fab
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2025-02-09 15:21:20 +0500

    правки путей

--- src/js/include.js
+++ src/js/include.js
@@ -10,20 +10,3 @@ function includeHTML() {
 }
 
 document.addEventListener("DOMContentLoaded", includeHTML);
-
-/* document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll("include").forEach(async (el) => {
-    const file = el.getAttribute("src");
-    if (file) {
-      try {
-        const response = await fetch(file);
-        if (!response.ok) throw new Error(`Ошибка загрузки файла: ${file}`);
-        const content = await response.text();
-        el.outerHTML = content;
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  });
-});
- */
