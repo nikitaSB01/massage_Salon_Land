7121e51d47487bd901caee3c497af28643fe0327
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2025-02-09 16:47:21 +0500

    сборка

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       targets: [
         { src: "src/components", dest: "" }, // Копируем компоненты
         { src: "src/js/include.js", dest: "js" }, // Копируем include.js
-        { src: "src/js/reviews.js", dest: "js" }, // Копируем include.js
+        { src: "src/js/reviews.js", dest: "js" }, // Копируем reviews.js
         { src: "src/js/service-details.js", dest: "js" }, // Добавили service-details.js
       ],
     }),
