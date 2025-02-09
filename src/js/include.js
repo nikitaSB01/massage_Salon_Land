12d8da2b7db3045e6faec3c1a204820fcde1e799
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2025-02-09 16:44:41 +0500

    правка путей для new стр

--- src/js/include.js
+++ src/js/include.js
@@ -1,12 +1,17 @@
 function includeHTML() {
-  document.querySelectorAll("[data-include]").forEach((el) => {
-    fetch(el.dataset.include)
-      .then((response) => response.text())
-      .then((data) => (el.innerHTML = data))
-      .catch((error) =>
-        console.error(`Ошибка загрузки ${el.dataset.include}:`, error)
-      );
+  document.querySelectorAll("[data-include]").forEach(async (el) => {
+    const file = el.getAttribute("data-include");
+    if (file) {
+      try {
+        const response = await fetch(file);
+        if (!response.ok) throw new Error(`Ошибка загрузки ${file}`);
+        el.innerHTML = await response.text(); // Вставляем содержимое
+      } catch (error) {
+        console.error("Ошибка загрузки компонента:", error);
+      }
+    }
   });
 }
 
+// Выполняем функцию после загрузки DOM
 document.addEventListener("DOMContentLoaded", includeHTML);
